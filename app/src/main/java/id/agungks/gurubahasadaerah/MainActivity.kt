8c4d5716c6c003717c20bbb8eb2fb18c4d4fc442
Agung Kurniawan Syahferdi <Agungsyahferdi69@gmail.com>
2020-05-14 02:13:10 +0700

    Menambahkan komponen GUSADA

---
+++ app/src/main/java/id/agungks/gurubahasadaerah/MainActivity.kt
@@ -0,0 +1,12 @@
+package id.agungks.gurubahasadaerah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
